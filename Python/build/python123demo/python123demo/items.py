f0c02da7bd8bc05160e6a8fc31aeef0bf4fb4294
YelonhimX <247574922@qq.com>
2019-10-24 07:52:57 +0800

    脑袋疼炸的网页分析和Scrapy

--- Python/build/python123demo/python123demo/items.py
+++ Python/build/python123demo/python123demo/items.py
@@ -10,5 +10,7 @@ import scrapy
 
 class Python123DemoItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
+    uid  = scrapy.Field()
+    
     pass
