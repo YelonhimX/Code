6a8e4b663005a6cdc656d44be443d43983b0e2bb
YelonhimX <247574922@qq.com>
2019-10-31 18:55:59 +0800

    Scrapy 工程更新

--- Python/build/marxspider/marxspider/pipelines.py
+++ Python/build/marxspider/marxspider/pipelines.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
+#- * -coding: utf - 8 - * -
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+#Define your item pipelines here## Don 't forget to add your pipeline to the ITEM_PIPELINES setting
+#See: https: //docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class MarxspiderPipeline(object):
-    def process_item(self, item, spider):
-       for ir in range(item["link"]):
-           print(item["link"][ir])
+class MarxspiderPipeline(object):   
+    def process_item(self, item, spider): 
+    #for ir in range(item["link"]):
+        print(item["link"])
         return item
--- Python/build/marxspider/marxspider/spiders/marx.py
+++ Python/build/marxspider/marxspider/spiders/marx.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from marxspider.items import MarxspiderItem
+from scrapy.http import HtmlResponse
+from scrapy.selector import Selector
 
 class MarxSpider(scrapy.Spider):
     name = 'marx'
@@ -8,9 +10,18 @@ class MarxSpider(scrapy.Spider):
     start_urls = ['https://marxists.org/chinese/marx-engels/01/index.htm']
 
     def parse(self, response):
+        url = 'https://marxists.org/chinese/marx-engels/01/'
         item = MarxspiderItem()
-        item["link"] = response.xpath("//a[@href]").extract()
-        
-            
-        yield item
-       
+        item["link"] = response.xpath("//a/@href").re(r'\d{3}.htm.*?')
+        #每个链接的编号，xpath+正则，其实这样麻烦了,scrapy麻烦就在于此，一个for循环就解决的问题。
+        for ir in item["link"]:
+            urls = url+ir
+            print(urls)
+        #print(len(item["link"]))
+        #调试用
+            #yield scrapy.Request(urls,callback=text_parse)
+            #回调函数，下一步爬取requests
+    
+    def text_parse(self, response):
+        #爬取每一个章节的文字
+        pass
